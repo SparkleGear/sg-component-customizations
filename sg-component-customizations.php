9d8c49703cfcd3d160cd41df699ba4478eb375b9
Jeff @ Pye Brook <jeff@pyebrook.com>
2019-10-23 09:28:17 -0400

    don't override priced individually or hide subtotal amount if not a SGF apparel or accessory

--- sg-component-customizations.php
+++ sg-component-customizations.php
@@ -14,13 +14,13 @@ function sg_woocommerce_composite_component_data( $data, $i, $composite ) {
 		$data['pagination_style'] = 'load-more';
 		$data['display_prices']   = 'absolute';
 		$data['show_orderby']     = 'yes';
+		$data['hide_subtotal_product'] = 'yes';
+		$data['priced_individually']   = 'yes';
 	}
 
 	// always do this
 	$data['hide_subtotal_cart']    = 'yes';
 	$data['hide_subtotal_orders']  = 'yes';
-	$data['hide_subtotal_product'] = 'yes';
-	$data['priced_individually']   = 'yes';
 
 	return $data;
 }
